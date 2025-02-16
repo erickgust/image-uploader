2a8f512c7e2bd2c8f1004a2f08273974f7f2a0a8
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-16 04:15:04 -0300

    fix: update title in metadata for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: 'ImageUploader',
+  title: 'ImageUpload',
   description: 'Upload images and share them with others.',
 }
 
